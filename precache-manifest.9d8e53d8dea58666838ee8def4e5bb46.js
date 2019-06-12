30eb06c99dd8b4b8a9ab3cfdf3c4e0036d63be4a
Ivan Deiko <ivandeiko1997@gmail.com>
2019-06-12 14:39:04 +0300

    Updates

---
+++ precache-manifest.9d8e53d8dea58666838ee8def4e5bb46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a570685c9bc401acf938d218120ad5c",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "ba7dbd46171445da4489",
+    "url": "/react_dynamic-list-of-posts/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "44edee4ca8c41c4a6979",
+    "url": "/react_dynamic-list-of-posts/static/js/2.625adabb.chunk.js"
+  },
+  {
+    "revision": "ba7dbd46171445da4489",
+    "url": "/react_dynamic-list-of-posts/static/js/main.e03fdff8.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
